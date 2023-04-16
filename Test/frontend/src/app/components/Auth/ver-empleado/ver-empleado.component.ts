f56e36fd6489856aef768c4ca9f13b5455616166
mictlangelo10 <miguehdzss29@gmail.com>
2023-04-16 10:26:22 -0600

    Hola

---
+++ Test/frontend/src/app/components/Auth/ver-empleado/ver-empleado.component.ts
@@ -0,0 +1,105 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/auth.service';
+import { TokenService } from 'src/app/service/token.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-ver-empleado',
+  templateUrl: './ver-empleado.component.html',
+  styleUrls: ['./ver-empleado.component.css'],
+})
+export class VerEmpleadoComponent implements OnInit {
+  isAdmin: boolean = false;
+  filterPost = '';
+  employees: any = [];
+  roles: any = [];
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private token: TokenService
+  ) {
+    this.authService.list().subscribe(
+      (resp) => {
+        console.log(resp);
+        this.employees = resp;
+        // this.capyfit.getOneRol(this.employees.IdEmpleado).subscribe(res => {
+        //   console.log(res);
+        // })
+      },
+      (err) => console.error(err)
+    );
+  }
+
+  ngOnInit(): void {
+    this.isAdmin = this.token.isAdmin();
+  }
+
+  deleteEmployee(employee: any) {
+    console.log(employee);
+    console.log(employee.EmployeeID);
+    Swal.fire({
+      title: 'Do you want to delete the employee?', //+this.selectedRole.Name,//Are you sure?
+      text: 'This action cannot be undone.', //+this.selectedRole.Name,
+      html: '<p><strong>Employee: </strong>' + employee.username + '</p>',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, do it',
+      confirmButtonColor: '#1a1a1a',
+      cancelButtonText: 'Cancel',
+      cancelButtonColor: '#b9b9b9',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: 'Done!',
+          text: 'You have deleted employee ' + employee.username,
+          icon: 'success',
+          confirmButtonText: 'OK',
+          confirmButtonColor: '#1a1a1a',
+        });
+        //this.deleteRole(role);
+        this.authService.delete(employee.id).subscribe(
+          (res) => {
+            console.log(res);
+          },
+          (err) => console.error(err)
+        );
+        this.router.navigate(['']);
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire({
+          title: 'Cancelled',
+          text: 'Employee not deleted',
+          icon: 'error',
+          confirmButtonText: 'OK',
+          confirmButtonColor: '#1a1a1a',
+        });
+      }
+    });
+  }
+
+  showQR(empQR: any) {
+    delete empQR.Password;
+    let data = JSON.stringify(empQR);
+    let encodedData = encodeURIComponent(data);
+    let api =
+      'https://api.qrserver.com/v1/create-qr-code/?data=' +
+      encodedData +
+      '&size=250x250';
+    console.log(api);
+
+    console.log(data);
+    Swal.fire({
+      title: 'Query QR',
+      html:
+        '<p>' +
+        empQR.nombreUsuario +
+        '</p><img src="' +
+        api +
+        '" height="250px" width="250px">', // height="50px" width="50px"
+      icon: 'info',
+      confirmButtonText: 'OK',
+      confirmButtonColor: '#1a1a1a',
+    });
+  }
+}
